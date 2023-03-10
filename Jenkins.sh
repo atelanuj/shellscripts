fb30780bfdccdabf58083f42cb16108e0a3ce07a
unknown <atelanuj123@gmail.com>
2023-03-10 16:11:23 +0530

    Jenkins installation Script added

---
+++ Jenkins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install openjdk-11-jdk -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc &gt; /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list &gt; /dev/null
+
+sudo apt update
+
+sudo apt install jenkins -y
