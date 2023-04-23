ca8fe33d88f1cc6469ad0ef4977de53469764113
Anuj Atel <122249665+atelanuj@users.noreply.github.com>
2023-04-23 22:08:09 +0530

    updateed the new jenkins script

--- Jenkins.sh
+++ Jenkins.sh
@@ -15,3 +15,23 @@ sudo apt install jenkins -y
 sudo systemctl status jenkins.service
 
 sudo ufw allow 8080
+
+############################################################################################
+
+sudo yum update
+
+sudo yum install openjdk-11-jdk -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo yum update
+
+sudo yum jenkins
+
+sudo yum allow 8080
+
